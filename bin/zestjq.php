52340fbeec769f3a04dfe9d6a22d25834222998a
C. Scott Ananian <cscott@cscott.net>
2026-06-03 17:58:07 -0400

    bin/zestjq.php: Add improved autoloader support
    Try harder to find the correct autoload file when installed as a binary.
    
    Change-Id: I90b98cc8940a053477daa6340f1f7082c574bccf

--- bin/zestjq.php
+++ bin/zestjq.php
@@ -2,6 +2,34 @@
 <?php
 declare( strict_types = 1 );
 
-require_once __DIR__ . '/../vendor/autoload.php';
+// Find the appropriate path for the composer autoloader
+if ( isset( $GLOBALS['_composer_autoload_path'] ) ) {
+	define( 'PHPUNIT_COMPOSER_INSTALL', $GLOBALS['_composer_autoload_path'] );
+
+	unset( $GLOBALS['_composer_autoload_path'] );
+} else {
+	foreach ( [ __DIR__ . '/../../autoload.php', __DIR__ . '/../vendor/autoload.php', __DIR__ . '/vendor/autoload.php' ] as $file ) {
+		if ( file_exists( $file ) ) {
+			define( 'PHPUNIT_COMPOSER_INSTALL', $file );
+
+			break;
+		}
+	}
+
+	unset( $file );
+}
+
+if ( !defined( 'PHPUNIT_COMPOSER_INSTALL' ) ) {
+	fwrite(
+		STDERR,
+		'You need to set up the project dependencies using Composer:' . PHP_EOL . PHP_EOL .
+		'    composer install' . PHP_EOL . PHP_EOL .
+		'You can learn all about Composer on https://getcomposer.org/.' . PHP_EOL
+	);
+
+	die( 1 );
+}
+
+require PHPUNIT_COMPOSER_INSTALL;
 
 exit( \Wikimedia\ZestJQ\JQCmd::main( $argc - 1, array_slice( $argv, 1 ) ) );
